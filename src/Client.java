e2e75ee7ea9603f3d52ec69b473604618ce94122
Nicholas Grant <ngrant40@gmail.com>
2015-12-18 01:37:47 -0800

    Updated with documentation, refactored server and client

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,4 @@
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
@@ -17,190 +18,204 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JFrame;
 
-
 public class Client extends JPanel implements ActionListener {
-	private boolean moveChosen, difficultyChosen;
-	private char choice, prediction;
-	private boolean noWin, playerWin, compWin;
-	private boolean done;
-	private int difficulty;
-	private int score, compScore, tie;
-	private JButton rock, paper, scissors; 
-	private JButton beginner, veteran;
-	private JButton exit;
-	
-	public Client() {
-		setBackground( Color.WHITE );
-		rock = new JButton( new ImageIcon("R.jpg") );
-		paper = new JButton( new ImageIcon("P.jpg") );
-		scissors = new JButton( new ImageIcon("S.jpg") );
-		beginner = new JButton("Beginner");
-		veteran = new JButton("Veteran");
-		exit = new JButton("Exit");
-		rock.addActionListener(this);
-		paper.addActionListener(this);
-		scissors.addActionListener(this);
-		beginner.addActionListener(this);
-		veteran.addActionListener(this);
-		exit.addActionListener(this);
-		add(beginner);
-		add(veteran);
-		add(rock);
-		add(paper);
-		add(scissors);
-		add(exit);
-		JFrame frame = new JFrame();
-		frame.add(this);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize( 800, 800 );
-		frame.setVisible( true );
-		choice = ' ';
-		prediction = ' ';
-		score = compScore = tie = 0;
-		moveChosen = difficultyChosen = false;
-		done = false;
-		difficulty = 1;
-	}
-	
-	public static void main(String[] args) {
-		Client client = new Client();
-		Socket sock;
-		BufferedReader in = null;
-		PrintStream out = null;
-		try{
-			sock = new Socket("localhost",1235);
-			System.out.println("Connected");
-			in = new BufferedReader(new InputStreamReader(
-			sock.getInputStream()));
-			out = new PrintStream(sock.getOutputStream());
-		} catch(Exception e) {
-			System.out.println(e.getMessage());
-		}
-		while (!client.difficultyChosen) {
-			try {
-				Thread.sleep( 50 );
-			} catch (InterruptedException e) {
-				System.out.println("Program interrupted.");
-			}
-		}
-		out.print(client.difficulty);
-		out.flush();
-		while (!client.done) {
-			client.repaint();
-			while (!client.moveChosen) {
-				try {
-					Thread.sleep( 50 );
-				} catch (InterruptedException e) {
-					System.out.println("Program interrupted.");
-				}
-			}
-			out.println(client.choice);
-			out.flush();
-			String move = "";
-			try {
-				move = in.readLine();
-			} catch (IOException e) {
-				System.out.println(e.getMessage());
-			}
-			client.prediction = move.charAt(0);
-			client.noWin = client.playerWin = client.compWin = false;
-			if ( client.choice == client.prediction ) {
-				client.tie++;
-				client.noWin = true;
-			}
-			else if ( ( client.choice == 'R' && client.prediction == 'P' ) ||
-					 ( client.choice == 'P' && client.prediction == 'S' ) ||
-					 ( client.choice == 'S' && client.prediction == 'R' )) {
-				client.compScore++;
-				client.compWin = true;
-			}
-			else {
-				client.score++;
-				client.playerWin = true;
-			}
-			client.moveChosen = false;
-		}
-	}
-	
-	public char getChoice() {
-		return choice;
-	}
-	
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		if (!difficultyChosen) {
-			g.setFont(new Font(g.getFont().getFontName(), Font.PLAIN, 26));
-			g.drawString("Choose a difficulty", 300, 250);
-		}
-		else {
-			g.setColor( Color.BLACK );
-			g.setFont(new Font(g.getFont().getFontName(), Font.PLAIN, 18));
-			g.drawString("You chose:", 340, 300);
-			if ( choice != ' ' ) {
-				try {
-					BufferedImage img = ImageIO.read(new File( choice + ".jpg"));
-					g.drawImage( img, 330, 310, this);
-				} catch (IOException e) {
-					System.out.println(e);
-				}
-			}
-		
-			g.drawString("The computer chose:", 300, 500);
-			if ( prediction != ' ' ) {
-				try {
-					BufferedImage img = ImageIO.read(new File( prediction + ".jpg"));
-					g.drawImage( img, 340, 510, this);
-				} catch (IOException e) {
-					System.out.println(e);
-				}
-			}
-			g.setFont(new Font(g.getFont().getFontName(), Font.PLAIN, 26));
-			if ( noWin ) {
-				g.drawString("You tied!", 340, 250);
-				noWin = false;
-			}
-			if ( playerWin ) {
-				g.drawString("You won!", 340, 250);
-				playerWin = false;
-			}
-			if ( compWin ){
-				g.drawString("Computer wins!", 340, 250);
-				compWin = false;
-			}
-			g.setFont(new Font(g.getFont().getFontName(), Font.PLAIN, 18));
-			g.drawString("Your score: " + score, 50, 650);
-			g.drawString("Computer score: " + compScore, 50, 700);
-			g.drawString("Tied: " + tie, 50, 750);
-		}
-	}
-	
-	@Override
-	public void actionPerformed(ActionEvent a) {
-		if (a.getSource() == beginner || a.getSource() == veteran) {
-			difficultyChosen = true;
-			if (a.getSource() == beginner) {
-				difficulty = 1;
-			}
-			else {
-				difficulty = 2;
-			}
-			remove(beginner);
-			remove(veteran);
-		}
-		else if (a.getSource() == exit) {
-			System.exit(0);
-		}
-		else {
-			moveChosen = true;
-			if (a.getSource() == rock) {
-				choice = 'R';
-			}
-			else if (a.getSource() == paper) {
-				choice = 'P';
-			}
-			else if (a.getSource() == scissors) {
-				choice = 'S';
-			}
-		}
-	}
+
+    private boolean moveChosen, difficultyChosen;
+    private char choice, prediction;
+    private boolean noWin, playerWin, compWin;
+    private boolean done;
+    private int difficulty;
+    private int score, compScore, tie;
+    private JButton rock, paper, scissors;
+    private JButton beginner, veteran;
+    private JButton exit;
+
+    public Client() {
+        // Initialization of GUI components
+        setBackground(Color.WHITE);
+        rock = new JButton(new ImageIcon("R.jpg"));
+        paper = new JButton(new ImageIcon("P.jpg"));
+        scissors = new JButton(new ImageIcon("S.jpg"));
+        beginner = new JButton("Beginner");
+        veteran = new JButton("Veteran");
+        exit = new JButton("Exit");
+        rock.addActionListener(this);
+        paper.addActionListener(this);
+        scissors.addActionListener(this);
+        beginner.addActionListener(this);
+        veteran.addActionListener(this);
+        exit.addActionListener(this);
+        add(beginner);
+        add(veteran);
+        add(rock);
+        add(paper);
+        add(scissors);
+        add(exit);
+        JFrame frame = new JFrame();
+        frame.add(this);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 800);
+        frame.setVisible(true);
+        
+        // Initialization of game logic variables
+        choice = ' ';
+        prediction = ' ';
+        score = compScore = tie = 0;
+        moveChosen = difficultyChosen = false;
+        done = false;
+        difficulty = 1;
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        
+        // Initialize means of communication to server
+        Socket sock;
+        BufferedReader in = null;
+        PrintStream out = null;
+        try {
+            sock = new Socket("localhost", 1235);
+            System.out.println("Connected");
+            in = new BufferedReader(new InputStreamReader(
+                    sock.getInputStream()));
+            out = new PrintStream(sock.getOutputStream());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        // Wait until user chooses difficulty
+        while (!client.difficultyChosen) {
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                System.out.println("Program interrupted.");
+            }
+        }
+        
+        // Send difficulty to server
+        out.print(client.difficulty);
+        out.flush();
+        
+        // Continue to play game until user presses "Exit"
+        while (!client.done) {
+            // Repaint the client for updated information
+            client.repaint();
+            
+            // Wait until user chooses a move
+            while (!client.moveChosen) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    System.out.println("Program interrupted.");
+                }
+            }
+            // Send user's move to server
+            out.println(client.choice);
+            out.flush();
+            String move = "";
+            
+            // read in AI's move
+            try {
+                move = in.readLine();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+            client.prediction = move.charAt(0);
+            
+            // Compare the player's move and the AI's move
+            // and add a point to a player accordingly
+            client.noWin = client.playerWin = client.compWin = false;
+            if (client.choice == client.prediction) {
+                client.tie++;
+                client.noWin = true;
+            } else if ((client.choice == 'R' && client.prediction == 'P')
+                    || (client.choice == 'P' && client.prediction == 'S')
+                    || (client.choice == 'S' && client.prediction == 'R')) {
+                client.compScore++;
+                client.compWin = true;
+            } else {
+                client.score++;
+                client.playerWin = true;
+            }
+            client.moveChosen = false;
+        }
+    }
+
+    public char getChoice() {
+        return choice;
+    }
+    
+    // Handles painting the GUI
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (!difficultyChosen) {
+            g.setFont(new Font(g.getFont().getFontName(), Font.PLAIN, 26));
+            g.drawString("Choose a difficulty", 300, 250);
+        } else {
+            g.setColor(Color.BLACK);
+            g.setFont(new Font(g.getFont().getFontName(), Font.PLAIN, 18));
+            g.drawString("You chose:", 340, 300);
+            if (choice != ' ') {
+                try {
+                    BufferedImage img = ImageIO.read(new File(choice + ".jpg"));
+                    g.drawImage(img, 330, 310, this);
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+            }
+
+            g.drawString("The computer chose:", 300, 500);
+            if (prediction != ' ') {
+                try {
+                    BufferedImage img = ImageIO.read(new File(prediction + ".jpg"));
+                    g.drawImage(img, 340, 510, this);
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+            }
+            g.setFont(new Font(g.getFont().getFontName(), Font.PLAIN, 26));
+            if (noWin) {
+                g.drawString("You tied!", 340, 250);
+                noWin = false;
+            }
+            if (playerWin) {
+                g.drawString("You won!", 340, 250);
+                playerWin = false;
+            }
+            if (compWin) {
+                g.drawString("Computer wins!", 340, 250);
+                compWin = false;
+            }
+            g.setFont(new Font(g.getFont().getFontName(), Font.PLAIN, 18));
+            g.drawString("Your score: " + score, 50, 650);
+            g.drawString("Computer score: " + compScore, 50, 700);
+            g.drawString("Tied: " + tie, 50, 750);
+        }
+    }
+    
+    // Handles any button clicks by the user
+    @Override
+    public void actionPerformed(ActionEvent a) {
+        if (a.getSource() == beginner || a.getSource() == veteran) {
+            difficultyChosen = true;
+            if (a.getSource() == beginner) {
+                difficulty = 1;
+            } else {
+                difficulty = 2;
+            }
+            remove(beginner);
+            remove(veteran);
+        } else if (a.getSource() == exit) {
+            System.exit(0);
+        } else {
+            moveChosen = true;
+            if (a.getSource() == rock) {
+                choice = 'R';
+            } else if (a.getSource() == paper) {
+                choice = 'P';
+            } else if (a.getSource() == scissors) {
+                choice = 'S';
+            }
+        }
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -10,89 +11,124 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-
 public class Server {
-	
-	public static void main(String[] args) {
-		ServerSocket server;
-		Socket sock;
-		BufferedReader in;
-		PrintWriter out;
-		Computer computer = new Computer();
-		char prediction;
-		final int PATTERN_SIZE = 5;
-		int info = 0;
-		int i = 1;
-		String p = " ";
-		int difficulty;
-		File f = new File("Computer.dat");
-		try {
-			server = new ServerSocket(1235);
-			System.out.println("Waiting...");
-			sock = server.accept();
-			System.out.println("Connected");
-			in = new BufferedReader(new InputStreamReader(
-			 sock.getInputStream()));
-			out = new PrintWriter(sock.getOutputStream());
-			difficulty = in.read();
-			if ( difficulty == '2' ) {
-				load(computer, f);
-			}
-			while (true) {
-				prediction = computer.makePrediction( new Pattern( p ) );
-				out.println(prediction);
-				out.flush();
-				String move = in.readLine();
-				if ( info < PATTERN_SIZE - 2 ) {
-					p += move;
-					info++;
-				}
-				else {
-					p = p.substring(0) + move;
-					computer.storePattern( new Pattern( p ) );
-					while ( ( PATTERN_SIZE - i ) > 2 ) {
-						computer.storePattern( 
-						 new Pattern( p.substring( 0, PATTERN_SIZE - i ) ) );
-						i++;
-					}
-					i = 1;
-					p = p.substring(1);
-				}
-			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		save(computer,f);
-	}
-	
-	public static void save( Computer computer, File f ) {
-		try {
-			ObjectOutputStream out = new ObjectOutputStream(
-			 new FileOutputStream ( f ));
 
-	        out.writeObject( computer );
-	        out.close();
-	        System.out.println("Computer saved.");
-	        
-	    } catch ( IOException e ) {
-	    	System.out.println("Error processing file.");
-	    }
-	}
-	
-	public static void load ( Computer computer, File f ) {
-		if ( f.exists() ) {
-			try {
-				ObjectInputStream in = new ObjectInputStream(
-                new FileInputStream( f ));
+    public static void main(String[] args) {
+        // Initialize sockets for communication between server-client
+        ServerSocket server;
+        Socket sock;
 
-                computer = ( Computer ) in.readObject();
+        // Initialize buffers for communication between server-client
+        BufferedReader in;
+        PrintWriter out;
+
+        // Initialize the AI player
+        Computer computer = new Computer();
+
+        // Initialize variables for game logic
+        char prediction;  // AI's move prediction
+        final int PATTERN_SIZE = 5; // longest pattern tracked by AI
+        int info = 0; // initial amount of information AI has
+        String p = " "; // initial pattern
+        int difficulty; // define difficulty (beginner or veteran)
+        File f = new File("Computer.dat"); // get file for computer
+
+        try {
+            // Setup connection between server and client
+            server = new ServerSocket(1235);
+            System.out.println("Waiting...");
+            sock = server.accept();
+            System.out.println("Connected");
+            in = new BufferedReader(new InputStreamReader(
+                    sock.getInputStream()));
+            out = new PrintWriter(sock.getOutputStream());
+
+            // Get the users difficulty choice and load if veteran
+            difficulty = in.read();
+            if (difficulty == '2') {
+                load(computer, f);
+            }
+
+            // Continue to run game until client exits
+            while (true) {
+                // AI makes prediction using most recent user pattern
+                prediction = computer.makePrediction(new Pattern(p));
+
+                // prediction is sent to client
+                out.println(prediction);
+                out.flush();
+
+                // user's move is read from client
+                String move = in.readLine();
+
+                // If AI doesn't have enough info to make a prediction, add
+                // user's move to pattern
+                if (info < PATTERN_SIZE - 2) {
+                    p += move;
+                    info++;
+                } // Else, make a new pattern out of the user's most recent move
+                // and the previous pattern
+                else {
+                    p = p.substring(0) + move;
+
+                    // store recent pattern
+                    computer.storePattern(new Pattern(p));
+                    
+                    // store all subpatterns
+                    for (int i = 1; (PATTERN_SIZE - i) > 2; i++) {
+                        computer.storePattern(
+                                new Pattern(p.substring(0, PATTERN_SIZE - i)));
+                    }
+                    
+                    // remove first move in pattern from the pattern
+                    p = p.substring(1);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        // save computer data
+        save(computer, f);
+    }
+    
+    /**
+     * Saves the computer's data for use in veteran mode
+     * @param computer      the computer being saved
+     * @param f             the file being saved to
+     */
+    public static void save(Computer computer, File f) {
+        try {
+            ObjectOutputStream out = new ObjectOutputStream(
+                    new FileOutputStream(f));
+
+            out.writeObject(computer);
+            out.close();
+            System.out.println("Computer saved.");
+
+        } catch (IOException e) {
+            System.out.println("Error processing file.");
+        }
+    }
+    
+    /**
+     * Loads the computer's data from file for use in veteran mode
+     * @param computer      the computer being loaded into
+     * @param f             the file being loaded from
+     */
+    public static void load(Computer computer, File f) {
+        if (f.exists()) {
+            try {
+                ObjectInputStream in = new ObjectInputStream(
+                        new FileInputStream(f));
+
+                computer = (Computer) in.readObject();
                 in.close();
                 System.out.println("Computer loaded.");
-            } catch ( IOException e) {
+            } catch (IOException e) {
                 System.out.println("Error processing file.");
-            } catch ( ClassNotFoundException c ) {
+            } catch (ClassNotFoundException c) {
                 System.out.println("Class not found.");
             }
-		}
-	}
+        }
+    }
 }
